8ead37ba0628309bbc206607a6f21367cc9ccacd
pmohamma <pmohamma@trinity.edu>
2019-01-16 11:59:12 -0500

    working on cleaning speeches

--- congressionalrecord/fileIterator.py
+++ congressionalrecord/fileIterator.py
@@ -1,18 +1,29 @@
 import os
 
-rootdir = 'C:/Users/pouya\python-projects\congressional-record\congressionalrecord\output'
+def main():
+	rootdir = 'C:/Users/pouya\python-projects\congressional-record\congressionalrecord\output'
 
-for subdir, dirs, files in os.walk(rootdir):
-  for file in files:
-    if file.endswith('.htm'):
-      fname = subdir + '\\'+ file
-      f = open(fname, 'r')
-      contents = ""
-      for line in f:
-        contents += line
-        #print (os.path.join(subdir, file))
-      f.close()
-      bodystart = contents.index('<body>') + 6
-      bodyend = contents.index('</body>')
-      contents = contents[bodystart:bodyend]
-      print(contents)
+	for subdir, dirs, files in os.walk(rootdir):
+	  for file in files:
+	    if file.endswith('.htm'):
+	      fname = subdir + '\\'+ file
+	      f = open(fname, 'r')
+	      contents = ""
+	      for line in f:
+	        contents += line
+	        #print (os.path.join(subdir, file))
+	      f.close()
+	      contents = cleanContents(contents)
+	      print(contents)
+
+def cleanContents(contents):
+  bodystart = contents.index('<body>') + 6
+  bodyend = contents.index('</body>')
+  ret = contents[bodystart:bodyend]
+  if (ret.index("<pre>") != -1):
+  	retstart = contents.index('<pre>') + 5
+  	retend = contents.index('</pre>')
+  	ret = ret[retstart:retend]
+  return ret
+
+main()
